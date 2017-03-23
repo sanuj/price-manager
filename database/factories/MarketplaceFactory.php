3364529b61c629c8643edff0971da1ae420e42fd
Rahul Kadyan <hi@znck.me>
2017-03-23 18:14:04 +0530

    Add marketplace model

---
+++ database/factories/MarketplaceFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Marketplace::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'website' => $faker->url,
+        'logo' => $faker->imageUrl(140, 140),
+        'group' => $faker->optional()->word,
+    ];
+});
---
+++ database/migrations/2017_03_23_123653_create_marketplaces_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMarketplacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marketplaces', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('website')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('group')->nullable();
+            $table->timestamps();
+
+            $table->index('group');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marketplaces');
+    }
+}
