a32957bb36fbdd04fd75f005aba33742305a1a2d
Rahul Kadyan <hi@znck.me>
2017-03-24 13:56:35 +0530

    Store encrypted credentials in database

--- app/CompanyMarketplace.php
+++ app/CompanyMarketplace.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 class CompanyMarketplace extends Pivot
 {
     protected $table = 'company_marketplace';
+
+    protected $decryptedCredentials;
+
+    public function getCredentialsAttribute()
+    {
+        if (is_null($this->decryptedCredentials)) {
+            $this->decryptedCredentials = json_decode(decrypt($this->attributes['credentials']), true);
+        }
+
+        return $this->decryptedCredentials;
+    }
+
+    public function setCredentialsAttribute(array $credentials)
+    {
+        $this->attributes['credentials'] = encrypt(json_encode($credentials));
+        $this->decryptedCredentials = null;
+    }
 }
