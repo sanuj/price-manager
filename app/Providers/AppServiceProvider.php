8a27f9c5aaf071ef6f2895dc7e3312072370f14c
Rahul Kadyan <hi@znck.me>
2017-03-24 18:12:50 +0530

    Add dusk & conditional provider registrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,14 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $testing = [
+        \Laravel\Dusk\DuskServiceProvider::class,
+    ];
+
+    protected $local = [
+        \Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+    ];
+
     /**
      * Bootstrap any application services.
      *
@@ -23,6 +31,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment('testing')) {
+            foreach ($this->testing as $provider) {
+                $this->app->register($provider);
+            }
+        }
+        if ($this->app->environment('local')) {
+            foreach ($this->local as $provider) {
+                $this->app->register($provider);
+            }
+        }
     }
 }
