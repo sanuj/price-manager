a191090e1732a77e082163d9486d1256bca09c6f
Rahul Kadyan <hi@znck.me>
2017-03-30 20:47:39 +0530

    Fix typo in config

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const { mix, config: Mix } = require('laravel-mix')
 
 global.Mix = Mix
 
-const publicPath = Mix.options.hmr ? 'http://localhost:8080/' : 'public'
+const publicPath = Mix.options.hmr ? 'http://localhost:8080/' : '/'
 
 mix.options({ extractVueStyles: 'css/app.css' })
     .js('resources/assets/js/app.js', 'public/js')
