7d8818d0a1ceecbb717f2843af54586969ccc51c
Sanuj Sharma <sanuj.sharma.in@gmail.com>
2017-05-12 01:58:46 +0530

    Tests for buy box share heurisitc (#48)
    * change parameters in buy box share heuristic price update
    
    * catch exception in price updater job
    
    * add factory for mongodb snapshots
    
    * remove unnecessary error reporting
    
    * fix snapshot's factory
    
    * add tests for buy box share function
    
    * Add mongodb in deps
    
    * Maybe mongo7.1
    
    * Update circle.yml
    
    * temp
    
    * make algorithm more customizable
    
    * clean up bugs and add new tests
    
    * add php mongodb driver
    
    * remove a redundant query
    
    * control update pricing switch by env file

--- app/Jobs/PriceUpdaterJob.php
+++ app/Jobs/PriceUpdaterJob.php
@@ -3,6 +3,8 @@
 namespace App\Jobs;
 
 use App\Company;
+use App\Exceptions\NoSnapshotsAvailableException;
+use App\Exceptions\NoSnapshotsWithOffersException;
 use App\Exceptions\ThrottleLimitReachedException;
 use App\Managers\MarketplaceManager;
 use App\Marketplace;
@@ -43,7 +45,7 @@ class PriceUpdaterJob extends SelfSchedulingJob
      */
     public function handle()
     {
-        error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE); // NOTICE: MWS SDK has deprecated code.
+
         $this->manager = resolve(MarketplaceManager::class);
 
         $this->debug('Running for company '.$this->company->name.'.');
@@ -55,6 +57,19 @@ class PriceUpdaterJob extends SelfSchedulingJob
                               ->chunk($this->getPerRequestCount(), function ($listings) {
                                   $this->updatePrice($listings);
                               });
+        } catch(NoSnapshotsAvailableException $e) {
+            // TODO: Too much repetitive code here.
+            $this->debug('NoSnapshotsAvailableException message: ' . $e->getMessage());
+            $marketplace_listing = $e->getMarketplaceListing();
+            $this->disableMarketplaceListing($marketplace_listing);
+            $this->reschedule();
+            return;
+        } catch(NoSnapshotsWithOffersException $e) {
+            $this->debug('NoSnapshotsAvailableException message: ' . $e->getMessage());
+            $marketplace_listing = $e->getMarketplaceListing();
+            $this->disableMarketplaceListing($marketplace_listing);
+            $this->reschedule();
+            return;
         } catch (ThrottleLimitReachedException $e) {
             $this->debug('Rescheduling, throttle limit reached.');
             $this->reschedule(60);
@@ -67,6 +82,15 @@ class PriceUpdaterJob extends SelfSchedulingJob
         }
 
         $this->reschedule(60 * $this->getFrequency());
+    }
+
+    protected function disableMarketplaceListing(MarketplaceListing $marketplace_listing) {
+        if($marketplace_listing) {
+            $marketplace_listing->status = 0;
+            $marketplace_listing->save();
+            // TODO: Increase log level and notify on slack.
+            $this->debug('Updated status = 0 of marketplace listing id = ' . $marketplace_listing->id);
+        }
     }
 
     /**
---
+++ tests/Unit/Pricing/Algorithms/BuyBoxShareHeuristicPriceTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Tests\Unit\Pricing\Algorithms;
+
+use App\Exceptions\NoSnapshotsAvailableException;
+use App\Exceptions\NoSnapshotsWithOffersException;
+use App\MarketplaceListing;
+use App\Mongo\Snapshot;
+use App\Pricing\Algorithms\BuyBoxShareHeuristicPrice;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class BuyBoxShareHeuristicPriceTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_it_can_calculate_buy_box_share() {
+        $marketplace_listing = $this->getMarketplaceListing();
+        $buy_box_share = $this->algorithm(true, $this->getSnapshots($marketplace_listing->id))
+            ->getBuyBoxShare($marketplace_listing);
+        $this->assertLessThanOrEqual(1, $buy_box_share);
+        $this->assertGreaterThanOrEqual(0, $buy_box_share);
+    }
+
+    public function test_it_can_handle_zero_snapshots() {
+        $this->expectException(NoSnapshotsAvailableException::class);
+        $marketplace_listing = $this->getMarketplaceListing();
+        $this->algorithm(true, $this->getSnapshots($marketplace_listing->id, [], [], 0))
+            ->getBuyBoxShare($marketplace_listing);
+    }
+
+    public function test_it_can_handle_snapshots_without_any_offers() {
+        $this->expectException(NoSnapshotsWithOffersException::class);
+        $marketplace_listing = $this->getMarketplaceListing();
+        $this->algorithm(true, $this->getSnapshots($marketplace_listing->id, []))
+            ->getBuyBoxShare($marketplace_listing);
+    }
+
+    public function test_it_can_increment_price() {
+        $selling_price = 800;
+        $predicted_price = $this->algorithm()->predict($this->getMarketplaceListing(100, $selling_price, 750, 900));
+        $this->assertEquals((2*0.05+1)*$selling_price, $predicted_price);
+    }
+
+    public function test_it_can_increment_price_with_custom_params() {
+        $selling_price = 800;
+        $multiplier = 4;
+        $increment_factor = 0.01;
+        $predicted_price = $this->algorithm()->predict($this->getMarketplaceListing(100, $selling_price, 750, 900, [
+            'params' => compact('increment_factor', 'multiplier')
+        ]));
+        $this->assertEquals(($multiplier*$increment_factor+1)*$selling_price, $predicted_price);
+    }
+
+    public function test_it_can_increment_price_for_medium_bbs() {
+        $selling_price = 800;
+        $predicted_price = $this->algorithm()->predict($this->getMarketplaceListing(50, $selling_price, 750, 900));
+        $this->assertEquals((0.05+1)*$selling_price, $predicted_price);
+    }
+
+    public function test_it_can_ceil_price() {
+        $selling_price = 800;
+        $max_price = 850;
+        $predicted_price = $this->algorithm()->predict($this->getMarketplaceListing(100, $selling_price, 750, $max_price));
+        $this->assertLessThanOrEqual((2*0.05+1)*$selling_price, $max_price);
+        $this->assertEquals($max_price, $predicted_price);
+    }
+
+    public function test_it_can_decrement_price() {
+        $selling_price = 800;
+        $predicted_price = $this->algorithm()->predict($this->getMarketplaceListing(1, $selling_price, 750, 900));
+        $this->assertEquals((1-0.05)*$selling_price, $predicted_price);
+    }
+
+    public function test_it_can_decrement_price_with_custom_params() {
+        $selling_price = 800;
+        $decrement_factor = 0.01;
+        $predicted_price = $this->algorithm()->predict($this->getMarketplaceListing(1, $selling_price, 750, 900, [
+            'params' => compact('decrement_factor')
+        ]));
+        $this->assertEquals((1-$decrement_factor)*$selling_price, $predicted_price);
+    }
+
+    public function test_it_can_floor_price() {
+        $selling_price = 800;
+        $min_price = 750;
+        $decrement_factor = 0.1;
+        $predicted_price = $this->algorithm()->predict($this->getMarketplaceListing(1, $selling_price, $min_price, 850, [
+            'params' => compact('decrement_factor')
+        ]));
+        $this->assertGreaterThanOrEqual((1-$decrement_factor)*$selling_price, $min_price);
+        $this->assertEquals($min_price, $predicted_price);
+    }
+
+    public function algorithm($mock_buy_box_snapshots = false, $snapshots = null) {
+        return $mock_buy_box_snapshots ?
+            $this->mockMethod('buyBoxSnapshots', $snapshots ?? $this->getSnapshots())
+            : $this->mockMethod('getBuyBoxShare', function (MarketplaceListing $marketplace_listing) {
+                switch($marketplace_listing->id) {
+                    case 1: return 0.1;
+                    case 100: return 0.8;
+                    default: return 0.5;
+                }
+            });
+    }
+
+    public function mockMethod($method_name, $return_value) {
+        $algorithm = $this->getMockBuilder(BuyBoxShareHeuristicPrice::class)
+            ->setMethods([$method_name])->getMock();
+        $algorithm->method($method_name)
+            ->will(is_callable($return_value) ? $this->returnCallback($return_value) : $this->returnValue($return_value));
+        return $algorithm;
+    }
+
+    public function getSnapshots($marketplace_listing_id=null, $offers=null, $competitors=null, $count=20) {
+        $params = [];
+        if(!is_null($marketplace_listing_id))
+            $params = array_merge($params, compact('marketplace_listing_id'));
+        if(!is_null($offers))
+            $params = array_merge($params, compact('offers'));
+        if(!is_null($competitors))
+            $params = array_merge($params, compact('competitors'));
+
+        return factory(Snapshot::class, $count)->make($params);
+    }
+
+    public function getMarketplaceListing($id=null, $marketplace_selling_price=null, $marketplace_min_price=null,
+                                          $marketplace_max_price=null, $repricing_algorithm=null) : MarketplaceListing {
+        $params = [];
+        if(!is_null($id))
+            $params = array_merge($params, compact('id'));
+        if(!is_null($marketplace_selling_price))
+            $params = array_merge($params, compact('marketplace_selling_price'));
+        if(!is_null($marketplace_min_price))
+            $params = array_merge($params, compact('marketplace_min_price'));
+        if(!is_null($marketplace_max_price))
+            $params = array_merge($params, compact('marketplace_max_price'));
+        if(!is_null($repricing_algorithm))
+            $params = array_merge($params, compact('repricing_algorithm'));
+
+        return factory(MarketplaceListing::class)->make($params);
+    }
+
+}
