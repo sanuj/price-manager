f6398824f020a5eaa7d02ac1babea5e7e3f76c24
Rahul Kadyan <hi@znck.me>
2017-05-13 22:14:11 +0530

    Add artisan command to import job

--- app/Console/Commands/ImportMarketplaceListingCommand.php
+++ app/Console/Commands/ImportMarketplaceListingCommand.php
@@ -2,7 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Company;
+use App\Jobs\ImportMarketplaceListingJob;
+use App\Marketplace;
+use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class ImportMarketplaceListingCommand extends Command
 {
@@ -11,7 +16,7 @@ class ImportMarketplaceListingCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'exponent:import';
+    protected $signature = 'exponent:import {company} {marketplace} {filename}';
 
     /**
      * The console command description.
@@ -24,9 +29,27 @@ class ImportMarketplaceListingCommand extends Command
      * Execute the console command.
      *
      * @return void
+     * @throws \Exception
      */
     public function handle()
     {
+        $company = Company::firstOrFail($this->argument('company'));
+        $marketplace = Marketplace::whereName($this->argument('marketplace'))->firstOrFail();
+        $filename = $this->resolve($this->argument('filename'));
 
+        if (!file_exists($filename)) {
+            throw new Exception($filename.' not found.');
+        }
+
+        dispatch(new ImportMarketplaceListingJob($company, $marketplace, $filename));
+    }
+
+    protected function resolve(string $filename)
+    {
+        if (Str::startsWith($filename, '/')) {
+            return realpath($filename);
+        }
+
+        return realpath(getcwd().DIRECTORY_SEPARATOR.$filename);
     }
 }
