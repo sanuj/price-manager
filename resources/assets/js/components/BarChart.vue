3f382e5d61b0352e684bbcd6e7ec2c00ebd1cafb
sanuj_s <sanuj.sharma.in@gmail.com>
2017-05-28 00:59:43 +0530

    add bar chart vue component

---
+++ resources/assets/js/components/BarChart.vue
@@ -0,0 +1,50 @@
+<script>
+import {Bar} from 'vue-chartjs'
+
+export default Bar.extend({
+  name: 'BarChart',
+  
+  props: {
+    barOne: {
+      type: Array,
+      required: true,
+    },
+
+    barLabels: {
+      type: Array,
+      required: true,
+    },
+
+    barX: {
+      type: Array,
+      required: true,
+    }
+  },
+
+  computed: {
+    data () {
+      return {
+        labels: this.barX,
+        datasets: [
+          {
+            label: this.barLabels[0],
+            backgroundColor: '#f87979',
+            data: this.barOne
+          }
+        ]
+      }
+    },
+  },
+  
+  mounted () {
+    this.renderChart(this.data, {responsive: true, maintainAspectRatio: false})
+  },
+
+  watch: {
+    data: function () {
+      this._chart.destroy()
+      this.renderChart(this.data, {responsive: true, maintainAspectRatio: false})
+    }
+  },
+})
+</script>
