30980d200530efc9fbdeb96b8b4616f74887fae6
Rahul Kadyan <hi@znck.me>
2017-05-30 11:51:16 +0530

    Prefetch scripts

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const { mix, config: Mix } = require('laravel-mix')
+const ChunkManifest = require('webpack-plugin-chunk-manifest')
 
 global.Mix = Mix
 
@@ -7,7 +8,20 @@ const publicPath = Mix.options.hmr ? 'http://localhost:8080/' : '/'
 if (mix.config.inProduction) mix.version()
 
 mix.options({ extractVueStyles: 'css/app.css' })
-    .js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/js/app.js', 'public/js/')
     .webpackConfig({
-      output: { publicPath }
+      output: { publicPath },
+      plugins: [
+          new ChunkManifest({
+            filename: 'chunk-manifest.json',
+            transform (chunks) {
+              Object.keys(chunks).forEach(key => {
+                chunks[key] = `/${chunks[key]}`.replace(/^(\/js\/\/)/g, '/')
+                console.log(chunks[key])
+              })
+
+              return chunks
+            }
+          })
+      ]
     })
