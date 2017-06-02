661effb1cad06173c5b3b0f7ad735cf574cd0335
Sanuj Sharma <sanuj.sharma.in@gmail.com>
2017-06-02 12:12:32 +0530

    Floor by average or median competitor price (#102)
    * add abstract class BasePrice
    
    * add floor predicted price which floors by avg or median competitor price
    
    * add tests for floor predicted price

--- app/Pricing/Algorithms/BuyBoxShareHeuristicPrice.php
+++ app/Pricing/Algorithms/BuyBoxShareHeuristicPrice.php
@@ -3,7 +3,6 @@
 namespace App\Pricing\Algorithms;
 
 use App\Contracts\PricingAlgorithmContract;
-use App\Exceptions\InvalidPriceException;
 use App\Exceptions\NoSnapshotsAvailableException;
 use App\Exceptions\NoSnapshotsWithOffersException;
 use App\MarketplaceListing;
@@ -12,15 +11,12 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Log;
 
-class BuyBoxShareHeuristicPrice implements PricingAlgorithmContract
+class BuyBoxShareHeuristicPrice extends BasePrice  implements PricingAlgorithmContract
 {
-    protected $min_price = 0;
-    protected $max_price = 0;
-    protected $selling_price = 0;
     protected $bbs_hours = 1;
     protected $bbs = null;
 
-    public function predict(MarketplaceListing $listing): float
+    public function calculatePrice(MarketplaceListing $listing, $silent=false): float
     {
         $this->init($listing);
 
@@ -38,33 +34,19 @@ class BuyBoxShareHeuristicPrice implements PricingAlgorithmContract
             }
         }
 
-        return $this->tamePredictedPrice($listing, $predicted_price);
+        if(!$silent) {
+            Log::debug(get_class($this).'::MarketplaceListingId('.$listing->id.') - bbs: '.$this->bbs
+                .', predicted_price: '.$predicted_price, $listing->repricing_algorithm['params'] ?? []);
+        }
+
+        return $predicted_price;
     }
 
     protected function init(MarketplaceListing $listing)
     {
-        $this->min_price = $listing->marketplace_min_price;
-        $this->max_price = $listing->marketplace_max_price;
-        $this->selling_price = $listing->marketplace_selling_price;
+        parent::init($listing);
         $this->bbs_hours = $listing->repricing_algorithm['params']['bbs_hours'] ?? $this->bbs_hours;
-        $this->validateListing($listing);
-
         $this->bbs = $this->getBuyBoxShare($listing, $this->bbs_hours);
-    }
-
-    protected function validateListing(MarketplaceListing $listing)
-    {
-        if ($this->min_price == 0) {
-            throw new InvalidPriceException($listing, 'min');
-        }
-
-        if ($this->max_price == 0 or $this->max_price < $this->min_price) {
-            throw new InvalidPriceException($listing, 'max');
-        }
-
-        if($this->selling_price == 0) {
-            throw new InvalidPriceException($listing, 'selling');
-        }
     }
 
     protected function buyBoxShareIsHigh()
@@ -110,12 +92,4 @@ class BuyBoxShareHeuristicPrice implements PricingAlgorithmContract
                        ->where('updated_at', '>', Carbon::now()->subMinutes($num_hours*60))
                        ->get();
     }
-
-    protected function tamePredictedPrice($listing, $predicted_price) {
-        Log::debug(get_class($this).'::MarketplaceListingId('.$listing->id.') - bbs: '.$this->bbs
-            .', predicted_price: '.$predicted_price, $listing->repricing_algorithm['params'] ?? []);
-
-        return min($this->max_price, max($predicted_price, $this->min_price));
-    }
-
 }
